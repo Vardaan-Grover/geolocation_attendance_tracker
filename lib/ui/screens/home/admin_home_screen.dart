5c877826609c57e20aa232e214951ad8841e88ac
Vardaan Grover <vankap.grover@gmail.com>
2024-09-29 17:51:26 +0530

    last checkpoint (started integration of background_locator_2 for gps tracking)

--- lib/ui/screens/home/admin_home_screen.dart
+++ lib/ui/screens/home/admin_home_screen.dart
@@ -1,10 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 import 'package:geolocation_attendance_tracker/constants.dart';
 import 'package:geolocation_attendance_tracker/models/company_model.dart';
-
+import 'package:geolocation_attendance_tracker/services/auth_functions.dart';
 import 'package:geolocation_attendance_tracker/services/firestore_functions.dart';
 import 'package:geolocation_attendance_tracker/models/user_model.dart';
+import 'package:geolocation_attendance_tracker/ui/screens/branch/view_branches_screen.dart';
 import 'package:geolocation_attendance_tracker/ui/widgets/home/add_branch_pathway_modal_sheet.dart';
 import 'package:geolocation_attendance_tracker/ui/widgets/home/title_button.dart';
 import 'package:geolocation_attendance_tracker/ui/widgets/home/user_info_header.dart';
@@ -19,12 +23,26 @@ class AdminHomeScreen extends StatefulWidget {
 }
 
 class _AdminHomeScreenState extends State<AdminHomeScreen> {
-  Company? company;
+  Stream<DocumentSnapshot<Map<String, dynamic>>>? _companyStream;
 
-  void onAddStaff() {
+  void startCompanyStream() {
+    final authUser = AuthFunctions.getCurrentUser();
+    if (authUser != null) {
+      setState(() {
+        _companyStream = FirebaseFirestore.instance
+            .collection('companies')
+            .doc(widget.user.associatedCompanyId)
+            .snapshots();
+      });
+    }
+  }
+
+  void onAddStaff() async {
+    final company =
+        await FirestoreFunctions.fetchCompany(widget.user.associatedCompanyId);
     final codes = {
       'Employee': company!.employeeCode,
-      if (widget.user.role == "super-admin") 'Admin': company!.adminCode,
+      if (widget.user.role == "super-admin") 'Admin': company.adminCode,
     };
     showAdaptiveDialog(
       context: context,
@@ -40,6 +58,7 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
             ),
           ],
           content: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               SizedBox(height: largeSpacing),
               ...codes.entries.map(
@@ -75,92 +94,101 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
     );
   }
 
-  void getCompany() async {
-    final fetchedCompany =
-        await FirestoreFunctions.fetchCompany(widget.user.associatedCompanyId);
-
-    if (fetchedCompany != null) {
-      setState(() {
-        company = fetchedCompany;
-      });
-    } else {
-      print('Company not found');
-    }
-  }
-
   @override
   void initState() {
     super.initState();
-    getCompany();
+    startCompanyStream();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text('Gelocation Attendance Tracker'),
-      ),
-      floatingActionButton: SizedBox(
-        width: 120,
-        height: 48,
-        child: FloatingActionButton(
-          onPressed: onAddStaff,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [Icon(Icons.add), Text('Add Staff')],
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text('Gelocation Attendance Tracker'),
+        ),
+        floatingActionButton: SizedBox(
+          width: 120,
+          height: 48,
+          child: FloatingActionButton(
+            onPressed: onAddStaff,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [Icon(Icons.add), Text('Add Staff')],
+            ),
           ),
         ),
-      ),
-      body: company == null
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Padding(
-              padding: const EdgeInsets.all(largeSpacing),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  UserInfoHeader(
-                    user: widget.user,
-                    company: company!,
-                  ),
-                  const SizedBox(height: mediumSpacing),
-                  const Divider(),
-                  const SizedBox(height: mediumSpacing),
-                  TitleButton(
-                    title: 'View Branches',
-                    icon: Icons.business,
-                    onPressed: () {},
-                  ),
-                  const SizedBox(height: mediumSpacing),
-                  TitleButton(
-                    title: 'Add a branch / office',
-                    icon: Icons.add_business,
-                    onPressed: () {
-                      showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        builder: (context) =>
-                            AddBranchPathwayModalSheet(widget.user),
+        body: _companyStream == null
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : StreamBuilder(
+                stream: _companyStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: const Text('Something went wrong'));
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  final snapshotData = snapshot.data;
+                  if (snapshotData != null) {
+                    final companyData = snapshotData.data();
+                    if (companyData != null && companyData.isNotEmpty) {
+                      final company = Company.fromFirestore(companyData);
+                      return Padding(
+                        padding: const EdgeInsets.all(largeSpacing),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [ 
+                            UserInfoHeader(
+                              user: widget.user,
+                              company: company,
+                            ),
+                            const SizedBox(height: mediumSpacing),
+                            const Divider(),
+                            const SizedBox(height: mediumSpacing),
+                            TitleButton(
+                              title: 'View Branches',
+                              icon: Icons.business,
+                              onPressed: () => Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      ViewBranchesScreen(company),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: mediumSpacing),
+                            TitleButton(
+                              title: 'Add a branch / office',
+                              icon: Icons.add_business,
+                              onPressed: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  isScrollControlled: true,
+                                  builder: (context) =>
+                                      AddBranchPathwayModalSheet(widget.user),
+                                );
+                              },
+                            ),
+                            const SizedBox(height: mediumSpacing),
+                            TitleButton(
+                              title: 'Attendance Reports',
+                              icon: Icons.table_chart_outlined,
+                              onPressed: () {},
+                            ),
+                            const SizedBox(height: mediumSpacing),
+                            TitleButton(
+                              title: 'Employee List',
+                              icon: Icons.list_alt,
+                              onPressed: () {},
+                            )
+                          ],
+                        ),
                       );
-                    },
-                  ),
-                  const SizedBox(height: mediumSpacing),
-                  TitleButton(
-                    title: 'Attendance Reports',
-                    icon: Icons.table_chart_outlined,
-                    onPressed: () {},
-                  ),
-                  const SizedBox(height: mediumSpacing),
-                  TitleButton(
-                    title: 'Employee List',
-                    icon: Icons.list_alt,
-                    onPressed: () {},
-                  )
-                ],
-              ),
-            ),
-    );
+                    }
+                  }
+                  return Center(child: const Text('Company not found'));
+                }));
   }
 }
